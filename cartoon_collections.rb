dce6893d1b68abcf2483afb385fccb0fbb7a1931
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-27 15:58:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    element.any?(valid_calls)
+    element == valid_calls[element]
   end
 end
