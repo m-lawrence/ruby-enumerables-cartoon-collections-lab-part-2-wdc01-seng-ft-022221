01c295d5fc990311555d6b767f32712043f6a12d
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-27 16:06:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,6 +18,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    element.include == valid_calls[i]
+    valid_calls.include?(element)
   end
 end
